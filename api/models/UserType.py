ec24854f5aad2660bad0b2e5fd97a9eb012f1455
bobohastler <bobohastler@gmail.com>
2023-07-15 11:44:01 +0300

    Доробив UserType

--- api/models/UserType.py
+++ api/models/UserType.py
@@ -3,4 +3,4 @@ from api import db, app
 class UserType(db.Model):
     __tablename__ = "UserType"
     id = db.Column(db.Integer, primary_key = True, nullable = False)
-    
+    name = db.Column(db.String(30), nullable = False)
